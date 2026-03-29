2c0545f7dd81040bec467eb83ef64c43dfd50b47
Akram Fattah <=>
2026-03-29 12:48:10 +0300

    edit method only post for post requests, and readme file

--- internal/handlers/verifyEmail.go
+++ internal/handlers/verifyEmail.go
@@ -16,6 +16,11 @@ type verifyEmailRequest struct {
 }
 
 func VerifyEmail(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
 
 	var input verifyEmailRequest
